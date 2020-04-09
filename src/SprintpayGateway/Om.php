4d4e342e6abe0cc31f1ddb05e3bdbd1254bff144
Sumbang Christian <tsumbang@outlook.com>
2020-04-09 08:40:01 +0100

    Ajout Orange Money

--- src/SprintpayGateway/Om.php
+++ src/SprintpayGateway/Om.php
@@ -4,34 +4,54 @@ namespace SprintpayGateway;
 
 class Om {
 
-  
-    public static function makePaiementFees($numero,$montant,$description,$nom,$prenom,$email,$produit,$pays,$apikey,$apisecret) {
+    public static function initialize($token,$merchantid,$phone,$orderid,$amount,$currency,$description,$companyname,$success,$failure,$notification) {
 
-        $curl = curl_init(); $today = date("Y-m-d H:i:s");
+        // initialisation du token
 
-        $config = new Config();
-
-        $datetime = $config->buildDateHeader(); 
-
-        $autorisation = $config->buildAuthorizationHeader($apikey,$apisecret,$datetime);
+        $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.sprint-pay.com/sprintpayapi/payment/orangemoney/request",
+            CURLOPT_URL => "https://api.orange.com/oauth/v2/token",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
+            CURLOPT_MAXREDIRS => 300,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "{\"phone\": \"".$numero."\",\r\n\"amount\": \"".$montant."\",\r\n\"description\": \"".$description."\",\r\n\"firstName\": \"".$nom."\",\r\n\"lastName\": \"".$prenom."\",\r\n\"emailId\": \"".$email."\",\r\n\"spMerchandRef1\": \"".$produit."\",\r\n\"spMerchandRef2\": \"".$produit."\",\r\n\"country\": \"".$pays."\"}",
+            CURLOPT_POSTFIELDS => "grant_type=client_credentials",
             CURLOPT_HTTPHEADER => array(
-                "authorization: ".$autorisation."",
-                "cache-control: no-cache",
-                "content-type: application/json",
-                "datetime: ".$datetime."",
+                "Authorization: Basic ".$token."",
+                "Content-Type: application/x-www-form-urlencoded"
             ),
         ));
-            
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        // initiation du paiement
+
+        $array_reponse = json_decode($response, true);
+
+        $token = $array_reponse["access_token"];
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.orange.com/orange-money-webpay/cm/v1/webpayment",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 300,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS =>"{\n  \"merchant_key\": \"".$merchantid."\",\n  \"currency\": \"".$currency."\",\n  \"order_id\": \"".$orderid."\",\n  \"amount\": ".$amount.",\n  \"return_url\": \"".$success."\",\n  \"cancel_url\": \"".$failure."\",\n  \"notif_url\": \"".$notification."\",\n  \"lang\": \"fr\",\n  \"reference\": \"Paiement prime\"\n}",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "Authorization: Bearer ".$token.""
+            ),
+        ));
+
         $response = curl_exec($curl);
 
         $err = curl_error($curl);
@@ -40,341 +60,26 @@ class Om {
 
         if ($err) {
 
-            $retour =  '{   "transactionID": "00000",
-                            "statusDesc": "'.$err.'",
-                            "amount": "'.$montant.'",
-                            "recieverNumber": null,
-                            "senderNumber": "'.$numero.'",
-                            "operationType": "MoMoRequestPayment",
-                            "statusCode": "00",
-                            "opComment": null,
-                            "processingNumber": "00000",
-                            "operationDate": "'.$today.'",
-                            "idSpMarchand": 0}';
+            $retour =  '{   
+                            
+                            "transaction_id": "00000",
+                            "status_desc": "Failed",
+                            "amount": "'.$amount.'",
+                            "payment_url": null,
+                            "status_code": 400,
+                            "description": "'.$err.'",
+                            "order_id": "00"
+                        
+                        }';
 
         } else {
 
             $retour = $response;
         }
 
-         return $retour;   
+        return $retour;
 
     }
-
-    public static function makePaiementFeesTest($numero,$montant,$description,$nom,$prenom,$email,$produit,$pays,$apikey,$apisecret) {
-
-        $curl = curl_init(); $today = date("Y-m-d H:i:s");
-
-        $config = new Config();
-
-        $datetime = $config->buildDateHeader(); 
-
-        $autorisation = $config->buildAuthorizationHeader($apikey,$apisecret,$datetime);
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://test-api.sprint-pay.com/sprintpayapi/payment/orangemoney/request",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "{\"phone\": \"".$numero."\",\r\n\"amount\": \"".$montant."\",\r\n\"description\": \"".$description."\",\r\n\"firstName\": \"".$nom."\",\r\n\"lastName\": \"".$prenom."\",\r\n\"emailId\": \"".$email."\",\r\n\"spMerchandRef1\": \"".$produit."\",\r\n\"spMerchandRef2\": \"".$produit."\",\r\n\"country\": \"".$pays."\"}",
-            CURLOPT_HTTPHEADER => array(
-                "authorization: ".$autorisation."",
-                "cache-control: no-cache",
-                "content-type: application/json",
-                "datetime: ".$datetime."",
-            ),
-        ));
-            
-        $response = curl_exec($curl);
-
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-
-            $retour =  '{   "transactionID": "00000",
-                            "statusDesc": "'.$err.'",
-                            "amount": "'.$montant.'",
-                            "recieverNumber": null,
-                            "senderNumber": "'.$numero.'",
-                            "operationType": "MoMoRequestPayment",
-                            "statusCode": "00",
-                            "opComment": null,
-                            "processingNumber": "00000",
-                            "operationDate": "'.$today.'",
-                            "idSpMarchand": 0}';
-
-        } else {
-
-            $retour = $response;
-        }
-
-         return $retour;   
-
-    }
- 
-    public static function makePaiement($numero,$montant,$description,$nom,$prenom,$email,$produit,$pays,$orderid, $ville, $company, $backurl, $notiyurl, $apikey,$apisecret) {
-
-        $today = date("Y-m-d H:i:s");
-
-        $config = new Config();
-
-        $datetime = $config->buildDateHeader(); 
-
-        $autorisation = $config->buildAuthorizationHeader($apikey,$apisecret,$datetime);
-
-        $curl = curl_init();
-
-        $curl = curl_init(); //$montantp = 50;
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.sprint-pay.com/sprintpayapi/payment/orangemoney/request/v2",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "{ 
-\"phone\":\"237".$numero."\",
-\"amount\":".$montant.",
-\"currency\":\"XAF\",
-\"description\":\"".$description."\",
-\"order_id\":\"".$orderid."\",
-\"firstName\":\"".$prenom."\", 
-\"lastName\":\"".$nom."\", 
-\"companyName\":\"".$company."\", 
-\"city\":\"".$ville."\",
-\"state\":\"\",
-\"country\":\"".$pays."\",
-\"address\":\"\",
-\"postal\":\"\", 
-\"emailId\":\"".$email."\", 
-\"spMerchandRef1\":\"".$produit."\", 
-\"spMerchandRef2\":\"".$produit."\",
-\"notify_url\":\"".$notifyurl."\",
-\"cancel_url\":\"".$backurl."\",
-\"return_url\":\"".$backurl."\",
-\"lang\":\"fr\"
-}",
-            CURLOPT_HTTPHEADER => array(
-                "Authorization: ".$autorisation."",
-                "Content-Type: application/json",
-                "DateTime: ".$datetime."",
-                "cache-control: no-cache"
-            ),
-        ));
-
-
-            $response = curl_exec($curl); $retour = array();
-
-            $err = curl_error($curl);
-    
-            curl_close($curl);
-
-            if ($err) {
-
-                $retour["statut"] = "failed"; $retour["message"] = $err;
-    
-            } else {
-    
-                $paiement = json_decode($response);
-
-                $retour["statut"] = "pending"; $retour["reference"] = $paiement->transactionID;
-
-                $retour["payurl"] = $paiement->payment_url;
-     
-                
-            }
-
-            return json_encode($retour);
-
-    }
-
-    public static function makePaiementTest($numero,$montant,$description,$nom,$prenom,$email,$produit,$pays,$orderid, $ville, $company, $backurl, $notiyurl, $apikey,$apisecret) {
-
-        $today = date("Y-m-d H:i:s");
-
-        $config = new Config();
-
-        $datetime = $config->buildDateHeader(); 
-
-        $autorisation = $config->buildAuthorizationHeader($apikey,$apisecret,$datetime);
-
-        $curl = curl_init();
-
-        $curl = curl_init(); //$montantp = 50;
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://test-api.sprint-pay.com/sprintpayapi/payment/orangemoney/request/v2",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => "{ 
-\"phone\":\"237".$numero."\",
-\"amount\":".$montant.",
-\"currency\":\"XAF\",
-\"description\":\"".$description."\",
-\"order_id\":\"".$orderid."\",
-\"firstName\":\"".$prenom."\", 
-\"lastName\":\"".$nom."\", 
-\"companyName\":\"".$company."\", 
-\"city\":\"".$ville."\",
-\"state\":\"\",
-\"country\":\"".$pays."\",
-\"address\":\"\",
-\"postal\":\"\", 
-\"emailId\":\"".$email."\", 
-\"spMerchandRef1\":\"".$produit."\", 
-\"spMerchandRef2\":\"".$produit."\",
-\"notify_url\":\"".$notifyurl."\",
-\"cancel_url\":\"".$backurl."\",
-\"return_url\":\"".$backurl."\",
-\"lang\":\"fr\"
-}",
-            CURLOPT_HTTPHEADER => array(
-                "Authorization: ".$autorisation."",
-                "Content-Type: application/json",
-                "DateTime: ".$datetime."",
-                "cache-control: no-cache"
-            ),
-        ));
-
-
-            $response = curl_exec($curl); $retour = array();
-
-            $err = curl_error($curl);
-    
-            curl_close($curl);
-
-            if ($err) {
-
-                $retour["statut"] = "failed"; $retour["message"] = $err;
-    
-            } else {
-    
-                $paiement = json_decode($response);
-
-                $retour["statut"] = "pending"; $retour["reference"] = $paiement->transactionID;
-
-                $retour["payurl"] = $paiement->payment_url;
-     
-                
-            }
-
-            return json_encode($retour);
-
-    }
-
-    public static function checkPaiement($transactionid, $apikey,$apisecret) {
-
-        $curl = curl_init();
-
-        $config = new Config();
-
-        $datetime = $config->buildDateHeader(); 
-
-        $autorisation = $config->buildAuthorizationHeader($apikey,$apisecret,$datetime);
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.sprint-pay.com/sprintpayapi/payment/orangemoney/check/v2?transaction=" . $transactionid . "",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "authorization: ".$autorisation."",
-                "cache-control: no-cache",
-                "content-type: application/json",
-                "datetime: ".$datetime."",
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        $err = curl_error($curl);
-
-        curl_close($curl); $retour = array();
-
-
-        if ($err) {
-
-            $retour["statut"] = "failed";
-            $retour["data"] = $response;
-
-        } 
-
-        else {
-
-            $retour["statut"] = "success";
-            $retour["data"] = $response;
-
-        }
-
-        return json_encode($retour);
-
-    }
-
-    public static function checkPaiementTest($transactionid, $apikey,$apisecret) {
-
-        $curl = curl_init();
-
-        $config = new Config();
-
-        $datetime = $config->buildDateHeader(); 
-
-        $autorisation = $config->buildAuthorizationHeader($apikey,$apisecret,$datetime);
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://test-api.sprint-pay.com/sprintpayapi/payment/orangemoney/check/v2?transaction=" . $transactionid . "",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "authorization: ".$autorisation."",
-                "cache-control: no-cache",
-                "content-type: application/json",
-                "datetime: ".$datetime."",
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        $err = curl_error($curl);
-
-        curl_close($curl); $retour = array();
-
-
-        if ($err) {
-
-            $retour["statut"] = "failed";
-            $retour["data"] = $response;
-
-        } 
-
-        else {
-
-            $retour["statut"] = "success";
-            $retour["data"] = $response;
-
-        }
-
-        return json_encode($retour);
-
-    }
-
 }
 
 ?>
